13a936b8eae3bb26088889081a3621c3f9c114ae
suer <suetsugu.r@gmail.com>
2012-05-27 23:04:21 +0900

    link

--- app/controllers/milkode_controller.rb
+++ app/controllers/milkode_controller.rb
@@ -14,9 +14,13 @@ class MilkodeController < ApplicationController
     @keyword = params[:keyword]
     option = FindGrep::FindGrep::DEFAULT_OPTION.dup
     option.dbFile = Dbdir.groonga_path(Dbdir.default_dir) 
-    option.packages = @project.repositories.map {|r| package_name(r)}
+    @name_map = repository_package_map
+    option.packages = @name_map.keys
     findGrep = FindGrep::FindGrep.new(@keyword, option)
-    @records = findGrep.pickupRecords
+    @results = []
+    findGrep.pickupRecords.each do |record|
+      @results << search_result(record)
+    end
   end
 
   private
@@ -26,10 +30,31 @@ class MilkodeController < ApplicationController
     render_404
   end 
 
+  def repository_package_map
+    @project.repositories.inject({}) do |h, repository|
+      h[package_name(repository)] = repository.identifier
+      h
+    end
+  end
+
   def package_name(repository)
     url = repository.url
     url.sub!(/\.git$/,"") if repository.type == 'Repository::Git'
     File.basename(url)
   end
 
+  def search_result(record)
+    {
+      :repository_identifier => repository_identifier(record[:shortpath]),
+      :path => filepath(record[:shortpath])
+    }
+  end
+
+  def repository_identifier(path)
+    @name_map[path.split(File::SEPARATOR).first]
+  end
+
+  def filepath(path)
+    path.split(File::SEPARATOR)[1..-1].join('/')
+  end
 end
