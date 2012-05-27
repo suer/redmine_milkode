74ca63b22129915f2735c325a0d1d0ac8ffbac76
suer <suetsugu.r@gmail.com>
2012-05-27 19:40:20 +0900

    search

--- app/controllers/milkode_controller.rb
+++ app/controllers/milkode_controller.rb
@@ -1,13 +1,18 @@
 require 'milkode/cdweb/lib/database'
+require 'milkode/findgrep/findgrep'
 
 class MilkodeController < ApplicationController
   unloadable
-
 
   def index
     @package_num = Database.instance.yaml_package_num
   end
 
   def search
+    @keyword = params[:keyword]
+    option = FindGrep::FindGrep::DEFAULT_OPTION.dup
+    option.dbFile = Dbdir.groonga_path(Dbdir.default_dir) 
+    findGrep = FindGrep::FindGrep.new(@keyword, option)
+    @records = findGrep.pickupRecords
   end
 end
