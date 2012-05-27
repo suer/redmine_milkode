cc41329bb798a07f0de4ce2b508a63a1a0d23e5d
mallowlabs <mallowlabs@gmail.com>
2012-05-27 10:07:55 +0900

    Added: search action

--- app/controllers/milkode_controller.rb
+++ app/controllers/milkode_controller.rb
@@ -1,7 +1,13 @@
+require 'milkode/cdweb/lib/database'
+
 class MilkodeController < ApplicationController
   unloadable
 
 
   def index
+    @package_num = Database.instance.yaml_package_num
+  end
+
+  def search
   end
 end
