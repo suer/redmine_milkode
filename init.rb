0de99d94bd9ff089d8453976876a931ea03996dd
mallowlabs <mallowlabs@gmail.com>
2012-06-15 18:20:59 +0900

    Added: README

--- init.rb
+++ init.rb
@@ -1,13 +1,13 @@
 Redmine::Plugin.register :redmine_milkode do
   name 'Milkode plugin'
-  author 'mallowlabs'
+  author 'suer'
   description 'This is a milkode plugin for Redmine'
   version '0.0.1'
-  url 'https://github.com/mallowlabs/redmine_milkode'
-  author_url 'https://github.com/mallowlabs/'
+  url 'https://github.com/suer/redmine_milkode'
+  author_url 'https://github.com/suer/'
 
   project_module :milkode do
     permission :milkode, {:milkode => [:index]}, :public => true
-    menu :project_menu, :milkode, { :controller => 'milkode', :action => 'index' }, :caption => :milkode#, :param => :project_id
+    menu :project_menu, :milkode, { :controller => 'milkode', :action => 'index' }, :caption => :milkode
   end
 end
